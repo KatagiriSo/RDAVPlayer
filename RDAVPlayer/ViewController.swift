a7d8162b1811152ef8fa8d2f7bbf6ea62235e182
katagiri <so@katagiri>
2016-08-10 10:50:35 +0900

    Initial Commit

---
+++ RDAVPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RDAVPlayer
+//
+//  Created by 片桐奏羽 on 2016/08/10.
+//  Copyright © 2016年 rodhosSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
